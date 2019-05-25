03de3e0c21065678d58579543268af2f76489462
Denys Sidorov <000scorpions000@gmail.com>
2019-05-25 07:26:18 +0300

    add first simple reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
 
+import windowSizeReducer from './window-resize-reducer';
 // config of reducers
 export default combineReducers({
-
+  windowSizeReducer
 });
---
+++ src/store/reducers/window-resize-reducer/index.js
@@ -0,0 +1,19 @@
+import { CHANGE_WINDOW_SIZE } from '../../action-variables/index';
+
+const initState = { value: 1200 };
+
+export default (state = initState, action) => {
+ switch (action.type) {
+  case CHANGE_WINDOW_SIZE:
+   // Log.info('Size screen: ' + state.value);
+   return { ...state, value: action.payload };
+   break;
+  default:
+   return state;
+ }
+};
+
+export const changeWindowSize = (size) => {
+ return { type: CHANGE_WINDOW_SIZE, payload: size };
+};
+
